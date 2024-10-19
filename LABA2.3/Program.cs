1e0380b11f218d612c06b907d3416bf501a3912d
Chloe3c <kristianishenko21@gmail.com>
2024-10-19 02:02:40 +0200

    Добавьте файлы проекта.

---
+++ LABA2.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+try
+{
+    //const double a = 45;
+    //const double b = 13;
+    //const double c = -23;
+
+    //const double a = -31;
+    //const double b = 65;
+    //const double c = 12;
+
+    const double a = 52;
+    const double b = -1;
+    const double c = -33;
+    if ((a < b)&&(a < c)) Console.WriteLine($"{a} (a - наименьшее)");
+    else if ((b < c)&&(b<a)) Console.WriteLine($"{b} (b - наименьшее)");
+    else if ((c<a)&&(c<b)) Console.WriteLine($"{c} (c - наименьшее)");
+    else Console.WriteLine("Какая то ошибка");
+    
+         
+    
+
+
+
+
+}
+catch
+{
+    Console.WriteLine("Введите верно!");
+}
