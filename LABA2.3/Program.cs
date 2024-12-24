20bddeacb43d2f7939e4a9463b0bd1670edac49b
Chloe3c <kristianishenko21@gmail.com>
2024-12-24 19:48:52 +0300

    Update Program.cs

--- LABA2.3/Program.cs
+++ LABA2.3/Program.cs
@@ -1,29 +1,48 @@
-try
+int a1 = 45, b1 = 13, c1 = -23;
+int a2 = -31, b2 = 65, c2 = 12;
+int a3 = 52, b3 = -1, c3 = -33;
+
+int min1 = a1;
+switch (b1 < min1)
 {
-    //const double a = 45;
-    //const double b = 13;
-    //const double c = -23;
-
-    //const double a = -31;
-    //const double b = 65;
-    //const double c = 12;
-
-    const double a = 52;
-    const double b = -1;
-    const double c = -33;
-    if ((a < b)&&(a < c)) Console.WriteLine($"{a} (a - наименьшее)");
-    else if ((b < c)&&(b<a)) Console.WriteLine($"{b} (b - наименьшее)");
-    else if ((c<a)&&(c<b)) Console.WriteLine($"{c} (c - наименьшее)");
-    else Console.WriteLine("Какая то ошибка");
-    
-         
-    
-
-
-
-
+    case true:
+        min1 = b1;
+        break;
 }
-catch
+switch (c1 < min1)
 {
-    Console.WriteLine("Введите верно!");
+    case true:
+        min1 = c1;
+        break;
 }
+Console.WriteLine("Наименьшее число из первой группы: " + min1);
+
+int min2 = a2;
+switch (b2 < min2)
+{
+    case true:
+        min2 = b2;
+        break;
+}
+switch (c2 < min2)
+{
+    case true:
+        min2 = c2;
+        break;
+}
+Console.WriteLine("Наименьшее число из второй группы: " + min2);
+
+int min3 = a3;
+switch (b3 < min3)
+{
+    case true:
+        min3 = b3;
+        break;
+}
+switch (c3 < min3)
+{
+    case true:
+        min3 = c3;
+        break;
+}
+Console.WriteLine("Наименьшее число из третьей группы: " + min3);
